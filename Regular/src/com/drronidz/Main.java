2f30af66ee5f7192691d2f63d0959d26b394b888
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-04 16:08:08 +0100

    issue done. closes #03.

--- Regular/src/com/drronidz/Main.java
+++ Regular/src/com/drronidz/Main.java
@@ -5,6 +5,9 @@ package com.drronidz;/*
     CREATED ON : 2:55 PM
 */
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -28,8 +31,8 @@ public class Main {
         System.out.println(alphanumeric.replaceAll("[aei]", "X"));
         System.out.println(alphanumeric.replaceAll("[aei]", "I replaced a letter here"));
         System.out.println(alphanumeric.replaceAll("[aei][Fj]", "I replaced a letter here"));
-
         System.out.println("harry".replaceAll("[Hh]arry","Harry"));
+
         System.out.println("-----------------------------------------------------------");
         System.out.println(alphanumeric.matches("^abcDeeeF12GhhabcDeeeiiiijkl99z"));
         System.out.println(alphanumeric);
@@ -37,11 +40,13 @@ public class Main {
         System.out.println(alphanumeric.replaceAll("[abcdef345678]", "X"));
         System.out.println(alphanumeric.replaceAll("[a-fA-F3-8]","X"));
         System.out.println(alphanumeric.replaceAll("(?i)[a-f3-8]", "X"));
+
         System.out.println("-----------------------------------------------------------");
         System.out.println(alphanumeric);
         System.out.println(alphanumeric.replaceAll("[0-9]","X"));
         System.out.println(alphanumeric.replaceAll("\\d","X"));
         System.out.println(alphanumeric.replaceAll("\\D","X"));
+
         System.out.println("-----------------------------------------------------------");
         String hasWhiteSpace = "I have blanks and\ta tab, and also a newline\n";
         System.out.println(hasWhiteSpace);
@@ -50,5 +55,30 @@ public class Main {
 //        System.out.println(hasWhiteSpace.replaceAll("\\S",""));
         System.out.println(hasWhiteSpace.replaceAll("\\w","X"));
         System.out.println(hasWhiteSpace.replaceAll("\\b","X"));
+
+        System.out.println("-----------------------------------------------------------\n");
+        String thirdAlphanumericString = "abcDeeeF12GhhabcDeeeiiiijkl99z";
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{3}", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe+", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe*", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{2,5}","YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("h+i*j", "Y"));
+
+        System.out.println("-----------------------------------------------------------\n");
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p> This is a paragraph about something.</p>");
+        htmlText.append("<p> This is a another paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p> Here is the summary.</p>");
+
+        String h2Pattern = ".*<h2>.*";
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
+
+
     }
 }
