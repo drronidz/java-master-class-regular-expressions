6da2513be5d3ce2ea9ed3dc5b78dd491374cc124
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-04 16:33:55 +0100

    issue done. closes #04.

--- Regular/src/com/drronidz/Main.java
+++ Regular/src/com/drronidz/Main.java
@@ -69,15 +69,42 @@ public class Main {
         StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
 
         htmlText.append("<h2>Sub-heading</h2>");
-        htmlText.append("<p> This is a paragraph about something.</p>");
-        htmlText.append("<p> This is a another paragraph about something else.</p>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is another paragraph about something else.</p>");
         htmlText.append("<h2>Summary</h2>");
-        htmlText.append("<p> Here is the summary.</p>");
+        htmlText.append("<p>Here is the summary.</p>");
 
-        String h2Pattern = ".*<h2>.*";
+        String h2Pattern = "<h2>";
         Pattern pattern = Pattern.compile(h2Pattern);
         Matcher matcher = pattern.matcher(htmlText);
         System.out.println(matcher.matches());
+
+        matcher.reset();
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+            System.out.println("Occurrence " + count + " : " + matcher.start() + " to " + matcher.end());
+        }
+        // Opening tag & Closing tag & everything between
+
+        String h2GroupPattern = "(<h2>.*?</h2>)";
+        Pattern groupPattern = Pattern.compile(h2GroupPattern);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+        System.out.println(groupMatcher.matches());
+        groupMatcher.reset();
+
+        while (groupMatcher.find()) {
+            System.out.println("Occurrence: " + groupMatcher.group(1));
+        }
+        System.out.println("-----------------------------------------------------------\n");
+
+        String h2TextGroups = "(<h2>)(.+?)(</h2>)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMatcher = h2TextPattern.matcher(htmlText);
+
+        while (h2TextMatcher.find()) {
+            System.out.println("Occurrence: " + h2TextMatcher.group(2));
+        }
 
 
     }
